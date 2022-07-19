fa4e9c20d5d8acb41d0c3db2b8adb835aeebcdee
nearTeacher <bondu.seo@gmail.com>
2022-07-19 18:10:10 +0900

    haha

---
+++ application/controllers/ApiController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace application\controllers;
+
+class ApiController extends Controller
+{
+    public function categoryList()
+    {
+        return $this->model->getCategoryList();
+    }
+
+    public function productInsert()
+    {
+        $json = getJson();
+        print_r($json);
+        return [_RESULT => $this->model->productInsert($json)];
+    }
+    public function productList2()
+    {
+        $result = $this->model->productList2();
+        return $result === false ? [] : $result;
+    }
+
+    public function productDetail()
+    {
+        $urlPaths = getUrlPaths();
+        if (!isset($urlPaths[2])) {
+            exit();
+        }
+        $param = [
+            'product_id' => intval($urlPaths[2])
+        ];
+        return $this->model->productDetail($param);
+    }
+}
